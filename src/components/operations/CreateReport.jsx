ebcb80d058390531e701cb18d276c1b55f7e3a98
Phunbyt <roluwafunbi@gmail.com>
2021-10-19 03:07:57 +0100

    operations report success screen

--- src/components/operations/CreateReport.jsx
+++ src/components/operations/CreateReport.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import BlueButton from "../general/BlueButton";
 import UploadDocument from "../general/UploadDocument";
 import UniqueNumberCard from "./subcomponents/UniqueNumberCard";
@@ -8,6 +8,8 @@ import SelectionField from "./subcomponents/SelectionField";
 import companyArray from "./subcomponents/companies";
 import OperationsDatePicker from "./subcomponents/OperationsDatePicker";
 import OperationsReportTable from "./subcomponents/OperationsReportTable";
+import OperationsContext from "../../contexts/operations/OperationsContext";
+import ReportNotificationModal from "./subcomponents/ReportNotificationModal";
 
 const uniqueCardData = [
    {
@@ -34,63 +36,72 @@ const selectionItems = [
 
 const placeholder = "None Selected";
 
-const CreateReport = () => (
-   <div className="past_agm">
-      <div className="dashboard_head">
-         <div className="operations_dashboard_header">
-            <h3>Reports</h3>
+const CreateReport = () => {
+   const { handleOpen } = useContext(OperationsContext);
+   return (
+      <div className="past_agm">
+         <div className="dashboard_head">
+            <div className="operations_dashboard_header">
+               <h3>Reports</h3>
+            </div>
+            <div className="fincon_dashboard_header">
+               {uniqueCardData.map((uniqueCard, index) => (
+                  <UniqueNumberCard
+                     key={index}
+                     text={uniqueCard.text}
+                     icon={uniqueCard.icon}
+                     bgColor={uniqueCard.bgColor}
+                  />
+               ))}
+            </div>
          </div>
-         <div className="fincon_dashboard_header">
-            {uniqueCardData.map((uniqueCard, index) => (
-               <UniqueNumberCard
-                  key={index}
-                  text={uniqueCard.text}
-                  icon={uniqueCard.icon}
-                  bgColor={uniqueCard.bgColor}
-               />
-            ))}
+
+         <div className="past_agm_company_info report_form_field">
+            <form action="">
+               <div className="selection_input_field">
+                  <p style={{ margin: 0 }}>Select a report</p>
+                  <SelectionField
+                     items={selectionItems}
+                     placeholder={placeholder}
+                  />
+               </div>
+               <div className="selection_input_field">
+                  <p style={{ margin: 0 }}>Choose Company</p>
+                  <SelectionField
+                     items={companyArray}
+                     placeholder={placeholder}
+                  />
+               </div>
+               <div className="selection_input_field">
+                  <p style={{ margin: 0 }}>Choose month to upload</p>
+                  <OperationsDatePicker />
+               </div>
+               <div className="selected_input_field">
+                  <p style={{ margin: 0 }}>
+                     <span>Company Name: </span>
+                     <span>Company Name: </span>
+                  </p>
+                  <p style={{ margin: 0 }}>
+                     <span>Report: </span>
+                     <span>Company Name: </span>
+                  </p>
+               </div>
+               <div className="past_agm_company_upload report_form_field_upload">
+                  <p>Upload document</p>
+                  <UploadDocument />
+               </div>
+
+               <OperationsReportTable />
+               <div className="submit_agm_report_request_btn">
+                       <BlueButton click={(e) => {
+                           e.preventDefault();
+                       handleOpen();}} buttonText="COMPLETE UPLOAD" />
+               </div>
+               </form>
+               <ReportNotificationModal/>
          </div>
       </div>
-
-      <div className="past_agm_company_info report_form_field">
-         <form action="">
-            <div className="selection_input_field">
-               <p style={{ margin: 0 }}>Select a report</p>
-               <SelectionField
-                  items={selectionItems}
-                  placeholder={placeholder}
-               />
-            </div>
-            <div className="selection_input_field">
-               <p style={{ margin: 0 }}>Choose Company</p>
-               <SelectionField items={companyArray} placeholder={placeholder} />
-            </div>
-            <div className="selection_input_field">
-               <p style={{ margin: 0 }}>Choose month to upload</p>
-               <OperationsDatePicker />
-            </div>
-            <div className="selected_input_field">
-               <p style={{ margin: 0 }}>
-                  <span>Company Name: </span>
-                  <span>Company Name: </span>
-               </p>
-               <p style={{ margin: 0 }}>
-                  <span>Report: </span>
-                  <span>Company Name: </span>
-               </p>
-            </div>
-            <div className="past_agm_company_upload report_form_field_upload">
-               <p>Upload document</p>
-               <UploadDocument />
-            </div>
-
-            <OperationsReportTable />
-            <div className="submit_agm_report_request_btn">
-               <BlueButton buttonText="COMPLETE UPLOAD" />
-            </div>
-         </form>
-      </div>
-   </div>
-);
+   );
+};
 
 export default CreateReport;
